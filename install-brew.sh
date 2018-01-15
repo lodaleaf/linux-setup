7ed51c9c97c33393675bc6f84c4ee3ba37d4576d
Lodaleaf <lodaleaf@gmail.com>
2018-01-15 22:50:02 +0700

    change default path to bashrc

--- install-brew.sh
+++ install-brew.sh
@@ -18,9 +18,9 @@ sudo apt-get install build-essential
 #    sudo yum groupinstall 'Development Tools'
 #  See http://linuxbrew.sh/#dependencies for more information.
 #- Add Linuxbrew to your ~/.profile by running
-echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.profile
-echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.profile
-echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.profile
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bashrc
+echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.bashrc
+echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.bashrc
 #- Add Linuxbrew to your PATH
 PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 #- We recommend that you install GCC by running:
