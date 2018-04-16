11b4dc17c2a7095e40950d4042985cae77168672
Lodaleaf <lodaleaf@gmail.com>
2018-04-16 22:16:06 +0700

    add python3

--- install-all.sh
+++ install-all.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y wget
 sudo apt-get install vim-nox -y
 sudo apt-get install -y tig
 sudo apt-get install -y terminator
+sudo apt-get install python3 -y
 sudo sh ./install-chrome.sh
 sudo sh ./install-oh-my-zsh.sh
 sudo sh ./install-brew.sh
